de238bd840d12d9483a8c6a2604282ffce941e1b
Lydia Rennert <lydia.rennert@gmail.com>
2026-09-15 22:28:21 +0200

    lre: Initial version

---
+++ src/main/java/guru/springframework/spring6coldservice/Spring6ColdServiceApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6coldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ColdServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(Spring6ColdServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/springframework/spring6coldservice/Spring6CoolServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6coldservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CoolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
